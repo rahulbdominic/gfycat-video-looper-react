d096891e75eaa2e6af2b9d42927654d3672bdb44
root <root@DESKTOP-T08N9D5.localdomain>
2017-08-19 22:24:20 -0700

    Added routing and page to go to specific gif

---
+++ src/UrlEntry.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+
+class UrlEntry extends Component {
+
+    constructor() {
+        super()
+        this.state={
+            url:"",
+            redirect: false
+        }
+        this.onChangeHandler = this.onChangeHandler.bind(this)
+        this.submitHandler = this.submitHandler.bind(this)
+        this.tryButtonHandler = this.tryButtonHandler.bind(this)
+    }
+
+    onChangeHandler(event) {
+        this.setState({url: "https://api.gfycat.com/v1test/gfycats/" + event.target.value})
+    }
+
+    submitHandler(event) {
+        this.setState({redirect:true})
+    }
+
+    tryButtonHandler(event) {
+        this.setState({
+            redirect: true,
+            url: "https://api.gfycat.com/v1test/gfycats/TautWhoppingCougar"
+        })
+    }
+
+    render() {
+        if(this.state.redirect) {
+            return (
+              <Redirect push to={{
+                pathname: 'view',
+                state: { url: this.state.url }
+              }} />
+            )
+        } else {
+            return (
+                <div style={{"padding":"20px"}}>
+                    <label>Name of Gfycat gif
+                        <input style={{display:"inline-block"}} onChange={this.onChangeHandler} />
+                    </label>
+                    <span style={{display:"block"}}>Eg: TautWhoppingCougar, BravePlainAmmonite</span>
+                    <button onClick={this.submitHandler}>Submit</button>
+                    <button onClick={this.tryButtonHandler} style={{"margin-left": "5px"}}>Try a sample</button>
+                </div>
+            )
+        }
+    }
+}
+
+export default UrlEntry
---
+++ src/VideoCanvas.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+
+class VideoCanvas extends Component {
+    constructor() {
+        super()
+
+        this.state={
+            startTime: 0,
+            endTime: 0,
+            duration: 0,
+            canvasWidth: 0,
+            canvasHeight: 0,
+            videoUrl: undefined,
+            aspectRatio: 0,
+            videoFrameRate: 30,
+            errorMessage: ""
+        }
+
+        this.looper = this.looper.bind(this)
+        this.loadedMetaDataHandler = this.loadedMetaDataHandler.bind(this)
+        this.startTimeChangeHandler = this.startTimeChangeHandler.bind(this)
+        this.endTimeChangeHandler = this.endTimeChangeHandler.bind(this)
+        this.resetPlayer = this.resetPlayer.bind(this)
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateWindowDimensions);
+        axios.get(this.props.urlToLoad)
+            .then(res => {
+                const obj = res.data.gfyItem;
+                this.setState({
+                    videoUrl: obj.mp4Url,
+                    canvasWidth: window.innerWidth,
+                    canvasHeight: window.innerWidth * obj.height / obj.width,
+                    aspectRatio: obj.height / obj.width,
+                    videoFrameRate: obj.frameRate
+                })
+            });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    looper () {
+        if(this.player == null || this.canvas == null) {
+            return;
+        }
+        const video = this.player
+        var ctx = this.canvas.getContext('2d')
+        if(video.currentTime < this.state.startTime) {
+            video.currentTime = this.state.startTime
+            video.play()
+        } else if (video.currentTime < this.state.endTime) {
+            ctx.drawImage(video, 0, 0, this.state.canvasWidth, this.state.canvasHeight)
+            setTimeout(this.looper, 1000 / this.state.videoFrameRate)
+        } else {
+            video.currentTime = this.state.startTime
+            setTimeout(this.looper, 1000 / this.state.videoFrameRate)
+        }
+    }
+
+    loadedMetaDataHandler (event) {
+        var duration = this.player.duration
+        this.setState({endTime: duration, duration: duration})
+
+        this.startTimeInput.value = this.state.startTime
+        this.endTimeInput.value = duration
+
+        this.player.currentTime = this.state.startTime
+        this.player.play()
+    }
+
+    startTimeChangeHandler(event) {
+        var _startTime = Number(event.target.value)
+        if (_startTime !== "NaN" && _startTime < this.state.duration && _startTime >= 0 && _startTime < this.state.endTime) {
+            this.setState({
+                startTime:_startTime,
+                errorMessage: ""
+            })
+            this.resetPlayer()
+        } else {
+            this.setState({
+                errorMessage:"Invalid input for one or more fields. \n" +
+                    "1)Start time and end time must be numbers \n" +
+                    "2)Start time must be >= 0 and end time must be <= " + this.state.duration + " \n" +
+                    "3)Start time must be <= end time \n"
+            })
+        }
+    }
+
+    endTimeChangeHandler(event) {
+        var _endTime = Number(event.target.value)
+        if (_endTime !== "NaN" && _endTime < this.state.duration && _endTime >= 0 && _endTime > this.state.startTime) {
+            this.setState({
+                endTime:_endTime,
+                errorMessage: ""
+            })
+            this.resetPlayer()
+        } else {
+            this.setState({
+                errorMessage:"Invalid input for one or more fields. \n" +
+                    "1)Start time and end time must be numbers \n" +
+                    "2)Start time must be >= 0 and end time must be <= " + this.state.duration + " \n" +
+                    "3)Start time must be <= end time \n"
+            })
+        }
+    }
+
+    resetPlayer() {
+        var _startTime = Number(this.startTimeInput.value)
+        var _endTime = Number(this.endTimeInput.value)
+        this.setState({
+            startTime: _startTime,
+            endTime: _endTime
+        })
+        this.player.pause()
+        this.player.currentTime = _startTime
+        this.player.play()
+    }
+
+    updateWindowDimensions() {
+        this.setState({
+            canvasWidth: window.innerWidth,
+            canvasHeight: window.innerWidth * this.state.aspectRatio
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <video onPlay={this.looper} onLoadedMetadata={this.loadedMetaDataHandler} src={this.state.videoUrl}
+                    controls="false" style={{display:'none'}} ref={(player) => {this.player = player}} autoPlay loop />
+
+                <canvas width={this.state.canvasWidth} height={this.state.canvasHeight} ref={(canvas) => {this.canvas = canvas}} />
+                <div className="Center">
+                    <label style={{display:"block"}}>
+                        Start time:
+                        <input style={{display:"inline-block"}} type="text" onChange={this.startTimeChangeHandler} ref={(input)=>{this.startTimeInput=input}} />
+                    </label>
+                    <label style={{display:"block"}}>
+                        End time:
+                        <input style={{display:"inline-block"}} type="text" onChange={this.endTimeChangeHandler} ref={(input)=>{this.endTimeInput=input}} />
+                    </label>
+                </div>
+                <span style={{color:'red', "white-space":"pre-line"}}>{this.state.errorMessage}</span>
+            </div>
+        )
+    }
+}
+
+VideoCanvas.propTypes = {
+    urlToLoad: PropTypes.string.isRequired
+}
+
+export default VideoCanvas
