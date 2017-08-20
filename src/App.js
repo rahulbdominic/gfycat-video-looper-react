29ac97b4f0b97350411e1c63e1f558df017a1709
root <root@DESKTOP-T08N9D5.localdomain>
2017-08-19 21:15:45 -0700

    Added basic styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types'
 import './App.css'
 import axios from 'axios'
 
-const videoCanvasStyle = {
-    width: "100%"
-}
-
 class VideoCanvas extends Component {
     constructor() {
         super()
@@ -79,7 +75,7 @@ class VideoCanvas extends Component {
 
     startTimeChangeHandler(event) {
         var _startTime = Number(event.target.value)
-        if (_startTime !== "NaN" && _startTime < this.state.duration && _startTime > 0 && _startTime < this.state.endTime) {
+        if (_startTime !== "NaN" && _startTime < this.state.duration && _startTime >= 0 && _startTime < this.state.endTime) {
             this.setState({
                 startTime:_startTime,
                 errorMessage: ""
@@ -88,16 +84,16 @@ class VideoCanvas extends Component {
         } else {
             this.setState({
                 errorMessage:"Invalid input for one or more fields. \n" +
-                    "1)Start time and end time must be numbers" +
-                    "2)Start time must be >= 0 and end time must be <=" + this.state.duration +
-                    "3)Start time must be <= end time"
+                    "1)Start time and end time must be numbers \n" +
+                    "2)Start time must be >= 0 and end time must be <= " + this.state.duration + " \n" +
+                    "3)Start time must be <= end time \n"
             })
         }
     }
 
     endTimeChangeHandler(event) {
         var _endTime = Number(event.target.value)
-        if (_endTime !== "NaN" && _endTime < this.state.duration && _endTime > 0 && _endTime > this.state.startTime) {
+        if (_endTime !== "NaN" && _endTime < this.state.duration && _endTime >= 0 && _endTime > this.state.startTime) {
             this.setState({
                 endTime:_endTime,
                 errorMessage: ""
@@ -106,9 +102,9 @@ class VideoCanvas extends Component {
         } else {
             this.setState({
                 errorMessage:"Invalid input for one or more fields. \n" +
-                    "1)Start time and end time must be numbers" +
-                    "2)Start time must be >= 0 and end time must be <=" + this.state.duration +
-                    "3)Start time must be <= end time"
+                    "1)Start time and end time must be numbers \n" +
+                    "2)Start time must be >= 0 and end time must be <= " + this.state.duration + " \n" +
+                    "3)Start time must be <= end time \n"
             })
         }
     }
@@ -139,17 +135,17 @@ class VideoCanvas extends Component {
                     controls="false" style={{display:'none'}} ref={(player) => {this.player = player}} autoPlay loop />
 
                 <canvas width={this.state.canvasWidth} height={this.state.canvasHeight} ref={(canvas) => {this.canvas = canvas}} />
-                <div>
-                    <label>
+                <div className="Center">
+                    <label style={{display:"block"}}>
                         Start time:
-                        <input type="text" onChange={this.startTimeChangeHandler} ref={(input)=>{this.startTimeInput=input}} />
+                        <input style={{display:"inline-block"}} type="text" onChange={this.startTimeChangeHandler} ref={(input)=>{this.startTimeInput=input}} />
                     </label>
-                    <label>
+                    <label style={{display:"block"}}>
                         End time:
-                        <input type="text" onChange={this.endTimeChangeHandler} ref={(input)=>{this.endTimeInput=input}} />
+                        <input style={{display:"inline-block"}} type="text" onChange={this.endTimeChangeHandler} ref={(input)=>{this.endTimeInput=input}} />
                     </label>
                 </div>
-                <span style={{color:'red'}}>{this.state.errorMessage}</span>
+                <span style={{color:'red', "white-space":"pre-line"}}>{this.state.errorMessage}</span>
             </div>
         )
     }
